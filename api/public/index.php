d7702f413472f8b50c5d871c4b61df21e48b6163
Juan Carlos <juanqui725@gmail.com>
2026-08-09 14:56:30 -0400

    corregido Cors  aiven 09/08/2026

--- api/public/index.php
+++ api/public/index.php
@@ -1,7 +1,8 @@
 <?php
-header('Access-Control-Allow-Origin: http://www.sexto2026.com');
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 if($_SERVER['REQUEST_METHOD']=='OPTIONS')
     {
